9e2e5e77d4ef35dd252d69dd68c34bf5c5c12fcf
Nick_En <95397912+nicktoooz@users.noreply.github.com>
2023-08-31 19:15:32 +0800

    Added multi-paged signup

--- public/firebase/sign-up.js
+++ public/firebase/sign-up.js
@@ -1,11 +1,24 @@
 const toPage2Btn = document.getElementById('nav-page-2');
-const page2 = document.querySelector('.page-2')
+const toPage3Btn = document.getElementById('nav-page-3');
+const toPage4Btn = document.getElementById('nav-page-4');
+const page2 = document.querySelector('.page-2');
+const page3 = document.querySelector('.page-3');
+const page4 = document.querySelector('.page-4');
 
 toPage2Btn.addEventListener('click', (e) => {
-    e.preventDefault();
-    if (page2.classList.contains('active')) {
-        page2.classList.remove('active')
-    } else {
-        page2.classList.add('active')
-    }
+  e.preventDefault();
+  if (page2.classList.contains('active')) {
+    page2.classList.remove('active');
+  } else {
+    page2.classList.add('active');
+  }
+});
+
+toPage3Btn.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (page3.classList.contains('active')) {
+    page3.classList.remove('active');
+  } else {
+    page3.classList.add('active');
+  }
 });
